8395c1b1a44223961fc276f93979bb24be0f649f
Nick McGuffin <nicholas.mcguffin@cqumail.com>
2023-10-13 08:14:39 +1000

    Addand cofnigure tailwind css

--- secret-swap/vite.config.ts
+++ secret-swap/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import browserslistToEsBuild from "browserslist-to-esbuild";
+import react from '@vitejs/plugin-react';
+import postcss from "./postcss.config.js";
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: browserslistToEsBuild(),
+  },
   plugins: [react()],
+  css: {
+    postcss
+  }
 })
