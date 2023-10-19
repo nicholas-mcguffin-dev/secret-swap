9cbae455dcd69bdf6ff7b0774cce9f602be09882
Nick McGuffin <nicholas.mcguffin@cqumail.com>
2023-10-19 22:58:34 +1000

    Convert footer text to black

--- secret-swap/src/components/Footer.tsx
+++ secret-swap/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className="bg-indigo-300 text-white py-2">
+        <footer className="bg-indigo-300 text-black py-2">
             <div className="container mx-auto text-center">
             <h6>&copy; {new Date().getFullYear()} Secret Swap</h6>
             </div>
