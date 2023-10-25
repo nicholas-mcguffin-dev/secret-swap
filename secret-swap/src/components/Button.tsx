13617c833b5a33f294b67dd351e9fdfaa7f285f0
Nick McGuffin <nicholas.mcguffin@cqumail.com>
2023-10-25 20:46:03 +1000

    Add set styles to button component
    Include the following styles for use across the app:
    Primary, Secondary, Tertiary, Success, Danger, Warning, Info

--- secret-swap/src/components/Button.tsx
+++ secret-swap/src/components/Button.tsx
@@ -1,22 +1,35 @@
 import { ButtonHTMLAttributes, ReactNode } from "react";
 
 type ButtonType = "button" | "submit" | "reset" | undefined;
+type ButtonHierarchy = "primary" | "secondary" | "tertiary" | "success" | "danger" | "warning" | "info";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
     children: ReactNode;
     disabled?: boolean;
     handleClick?: () => void;
-    styles: string;
     type?: ButtonType;
+    hierarchy: ButtonHierarchy;
     title: string;
 }
 
+const styles = {
+    "primary": "bg-indigo-500 text-white",
+    "secondary": "bg-white text-indigo-500 outline outline-2",
+    "tertiary": "text-indigo-500",
+    "success": "bg-success",
+    "danger": "bg-danger",
+    "warning": "bg-warning",
+    "info": "bg-info",
+}
+
 function Button(props: ButtonProps) {
+    const style = styles[props.hierarchy];
+
     return (
         <button
             onClick={props?.handleClick}
-            className={`${props.styles} middle none center rounded-lg bg-indigo-500 py-2 px-4 font-sans text-xs font-bold uppercase text-white shadow-md shadow-indigo-500/20 transition-all hover:shadow-lg hover:shadow-indigo-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none`}
-            type={props?.type}
+            className={`${style} rounded-lg py-2 px-4 font-sans text-xs font-bold uppercase text-white shadow-md shadow-indigo-500/20 transition-all hover:shadow-lg hover:shadow-indigo-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none`}
+            type={props?.type || "button"}
             disabled={props?.disabled}
         >
             {props.children}
